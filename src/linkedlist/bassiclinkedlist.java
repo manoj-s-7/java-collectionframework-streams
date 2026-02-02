955e6489c0c35a8865fc3e8c74211e2edd3ca102
Manoj <manojcs6317@gmail.com>
2026-02-02 19:30:58 +0530

    compeleted linked list

---
+++ src/linkedlist/bassiclinkedlist.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class bassiclinkedlist {
+    static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.get(2); //O(n)
+
+        list.addLast(6);
+        list.addFirst(0);
+        list.add(5,5);
+
+        System.out.println(list);
+        list.removeIf(e-> e%2 == 0 );
+        System.out.println(list);
+
+        LinkedList<String> animals = new LinkedList<>(Arrays.asList("cat","dog","lion","tiger"));
+        LinkedList<String> animalstoremove = new LinkedList<>(Arrays.asList("cat","dog"));
+        animals.removeAll(animalstoremove);
+        System.out.println(animals);
+    }
+}
