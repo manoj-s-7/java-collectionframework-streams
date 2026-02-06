918951dc05fd65dd8f2f4890ec675f386601ef43
Manoj <manojcs6317@gmail.com>
2026-02-06 21:30:39 +0530

    COMEPLETED BASICS of Streams

---
+++ src/java8Features/Mathoperation.java
@@ -0,0 +1,4 @@
+package java8Features;
+
+public interface Mathoperation {
+}
---
+++ src/java8Features/PredicateDemo.java
@@ -0,0 +1,8 @@
+package java8Features;
+
+public class Predicate {
+    static void main() {
+//        functional interface (boolean valued function
+
+    }
+}
